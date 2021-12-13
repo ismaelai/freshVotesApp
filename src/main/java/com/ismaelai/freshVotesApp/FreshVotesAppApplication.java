02014256f0a71dc8aff7139a288dc7c2bd0a35bf
ismaelai <ismamontiel@hotmail.com>
2021-12-13 16:11:16 +0100

    Initial commit

---
+++ src/main/java/com/ismaelai/freshVotesApp/FreshVotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.ismaelai.freshVotesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVotesAppApplication.class, args);
+	}
+
+}
