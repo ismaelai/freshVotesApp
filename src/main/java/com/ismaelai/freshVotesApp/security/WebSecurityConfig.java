daa295e45bb1350388a005c5ef47560dcdab3a15
ismaelai <ismamontiel@hotmail.com>
2021-12-14 17:44:18 +0100

    controller

--- src/main/java/com/ismaelai/freshVotesApp/security/WebSecurityConfig.java
+++ src/main/java/com/ismaelai/freshVotesApp/security/WebSecurityConfig.java
@@ -1,19 +1,28 @@
 package com.ismaelai.freshVotesApp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
+                .passwordEncoder(getPasswordEncoder())
                 .withUser("ismamontiel@hotmail.com")
-                .password("123456")
+                .password(getPasswordEncoder().encode("1234567"))
                 .roles("USER");
     }
 
---
+++ src/main/java/com/ismaelai/freshVotesApp/web/DashboardController.java
@@ -0,0 +1,19 @@
+package com.ismaelai.freshVotesApp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/")
+    public String rootView() {
+        return "index";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dashboard";
+    }
+
+}
