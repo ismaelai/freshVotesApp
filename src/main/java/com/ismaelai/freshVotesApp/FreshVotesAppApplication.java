49338d90f2ce8139b056c7882e82bde2fc175c88
ismaelai <ismamontiel@hotmail.com>
2021-12-14 16:09:29 +0100

    database tables

--- src/main/java/com/ismaelai/freshVotesApp/FreshVotesAppApplication.java
+++ src/main/java/com/ismaelai/freshVotesApp/FreshVotesAppApplication.java
@@ -3,11 +3,13 @@ package com.ismaelai.freshVotesApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FreshVotesAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FreshVotesAppApplication.class, args);
+		System.out.println("spring arrancado");
 	}
 
 }
